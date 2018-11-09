0415c8fa9468c653724cf1a013f568966d29f0d9
brenner-08 <43213427+brenner-08@users.noreply.github.com>
2018-11-09 13:54:59 -0500

    Update and rename bloomfilter20151428D.R to bloomfilter.R

---
+++ bloomfilter.R
@@ -0,0 +1,61 @@
+#BLOOMFILTER: 
+library(digest)
+library(magrittr)
+library(gmp)
+library(Rmpfr)
+
+vbits <- function(x,m=1000,k=7){
+  vec <- rep(FALSE,m)
+  for (i in 1:k) {
+    for (j in 1:length(x)) {
+      hash <- digest(x[j], algo = "murmur32", serialize = FALSE, seed = i) %>%
+        Rmpfr::mpfr(base = 16) %% 
+        m %>%
+        as.integer()
+      
+      vec[hash + 1] <- TRUE
+    }
+  }
+  attributes(vec) <- list(m = m, k= k)
+  
+  return(vec)
+}
+  
+vect <- vbits(c("Brenner", "jesus", "lisi", "Brian", "raul","juan", "rober", 
+                  "josé","Maria", "marco", "susana","jordi"), m = 1000, k = 7)
+#escojí estos nombres,donde entre ellos pueden ser personas maliciosas y otras no. y mediante este programa separamos(filtramos)
+#A las personas buenas de las malas
+#Así como El navegador web Google Chrome que solía usar un filtro Bloom para identificar URL maliciosas
+#Otros ejemplos de estos son:
+#Google BigTable, Apache HBase y Apache Cassandra, y Postgresql 
+#usan filtros Bloom para reducir las búsquedas en el disco para filas o columnas no existentes
+
+is_vect <- function(x, vbits){
+  k <- attr(vbits,"k")
+  m <- attr(vbits,"m")
+  
+  for (i in 1:k) {
+    hash <- digest(x,algo="murmur32",serialize = FALSE, seed=i) %>%
+      Rmpfr::mpfr(base = 16)%%
+      m %>%
+      as.integer()
+    if(!vbits[hash + 1]){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
+
+is_vect("Juan",vect)
+is_vect("Bruno",vect)
+is_vect("jose",vect)
+is_vect("Luis",vect)
+is_vect("pepe",vect)
+#y estos nombres son las personas a los que se quiere encontrar y ver si pertenecen dentro de la 
+#base de datos previamente insertados como: vect <- vbits(c("Brenner", "jesus", "lisi", "Brian", "raul","juan", "rober", 
+#"josé","Maria", "marco", "susana","jordi").
+
+
+
+
+
