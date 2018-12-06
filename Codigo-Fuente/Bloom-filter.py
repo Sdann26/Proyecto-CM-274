3dfdaa270e287d506808fa76a51d34b14bbc123a
andyaguilar <afranco582@gmail.com>
2018-12-06 12:44:16 -0500

    agregando comentarios iniciales a Bloom

---
+++ Codigo-Fuente/Bloom-filter.py
@@ -0,0 +1,9 @@
+#Codigo python del programa Blomm-filter
+#Es necesario instalar los siguientes modulos
+#pip install mmh3
+#pip install bitarray
+import math
+import mmh3
+from bitarray import bitarray
+
+print('hola prueba')
